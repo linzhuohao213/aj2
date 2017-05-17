8a1f1513d4d0ef9c87eb7fd31557dfe044b99853
linzhuohao213 <109441516@qq.com>
2017-05-17 15:04:27 +0800

    修改

--- my-app1/src/app/app.component.ts
+++ my-app1/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import {Hero} from './hero';
+import {HeroService} from './hero.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[HeroService]
 })
-export class AppComponent {
-  title = 'app works!';
+
+export class AppComponent implements OnInit {
+  constructor(private heroService:HeroService){
+
+  }
+  ngOnInit(): void {
+    this.getHeros();
+  }
+  title = '欢迎召唤师来到英雄峡谷';
+  heroes: Hero[];
+  selectedHero: Hero;
+  selectHero(hero:Hero):void{
+  	 this.selectedHero = hero;
+  }
+  getHeros():void{
+    this.heroService.getHeros().then(heros=>this.heroes=heros);
+  }
+
 }
+
+
---
+++ my-app1/src/app/hero.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROS } from './mock-heroes';
+
+
+@Injectable()
+export class HeroService {
+	getHeros():Promise<Hero[]>{
+		return Promise.resolve(HEROS);
+	}
+
+	getHerosSlow():Promise<Hero[]>{
+		return new Promise(resolve => {
+		    // Simulate server latency with 2 second delay
+		    setTimeout(() => resolve(this.getHeros()), 2000);
+		});
+	}
+}
